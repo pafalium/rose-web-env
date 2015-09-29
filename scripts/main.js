cf8200cdddc33a849e10cdb7926a43c6e7dd4841
pafalium <pedroalfaiate1992@hotmail.com>
2015-09-29 16:04:02 +0100

    Main: Example of setting the scene of the view.

--- scripts/main.js
+++ scripts/main.js
@@ -27,8 +27,15 @@ require(["app/page", "app/eval-js-to-three"], function(page, evaluator) {
             the actual user interface assembly (or do it itself);
     */
 
-    var program = page.editor.getValue();
-    var resultScene = evaluator.executeProgram(program);
-    page.view.showScene(resultScene);
+    //var program = page.editor.getValue();
+    //var resultScene = evaluator.executeProgram(program);
+    //page.view.showScene(resultScene);
+
+    var cube_geom = new THREE.BoxGeometry(1,1,1);
+    var mat = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var cube = new THREE.Mesh(cube_geom, mat);
+    var scene = new THREE.Scene();
+    scene.add(cube);
+    page.view.setScene(scene);
 
 });
