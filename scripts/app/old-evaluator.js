c320e898eb1b84e361c73a4400cd989fff3d7d82
pafalium <pedroalfaiate1992@hotmail.com>
2015-09-29 19:55:51 +0100

    old-evaluator:  - Added the rest of the primitives to support the examples from the past.  - Changed the way objects that use always the same geometry are scaled. This fixes problems when such objects are scaled later in the program.

--- scripts/app/old-evaluator.js
+++ scripts/app/old-evaluator.js
@@ -19,6 +19,14 @@ define(["THREE"], function(THREE){
 			};
 		})();
 
+		var surfaceMaterial = (function() {
+			var theSurfaceMaterial = new THREE.MeshLambertMaterial();
+			theSurfaceMaterial.side = THREE.DoubleSide;
+			return function surfaceMaterial() {
+				return theSurfaceMaterial;
+			};
+		})();
+
 		var cylinder = (function(){
 			var theCylinderGeometry = new THREE.CylinderGeometry( 1, 1, 1, 32 );
 			var makeCylinderMesh = function() {
@@ -29,7 +37,7 @@ define(["THREE"], function(THREE){
 			};
 			return function cylinder(r, h) {
 				var mesh = makeCylinderMesh();
-				mesh.scale.set(r,h,r);
+				scale(mesh, r,h,r);
 				addToScene(mesh);
 				return mesh;
 			};
@@ -45,7 +53,7 @@ define(["THREE"], function(THREE){
 			};
 			return function sphere(r) {
 				var mesh = makeSphereMesh();
-				mesh.scale.set(r,r,r);
+				scale(mesh, r,r,r);
 				addToScene(mesh);
 				return mesh;
 			};
@@ -61,15 +69,66 @@ define(["THREE"], function(THREE){
 			};
 			return function box(w,h,d){
 				var mesh = makeBoxMesh();
-				mesh.scale.set(w,h,d);
+				scale(mesh, w,h,d);
 				addToScene(mesh);
 				return mesh;
 			};
 		})();
 
+		var cone_frustum = function cone_frustum(baseRadius, height, topRadius) {
+			function makeConeFrustumGeometry(baseRadius, height, topRadius) {
+				return new THREE.CylinderGeometry(topRadius, baseRadius, height, 32);
+			}
+			function makeConeFrustumMesh(baseRadius, height, topRadius) {
+				var geom = makeConeFrustumGeometry(baseRadius, height, topRadius);
+				var mat = solidMaterial();
+				var mesh = new THREE.Mesh(geom, mat);
+				return mesh;	
+			}
+			var mesh = makeConeFrustumMesh(baseRadius, height, topRadius);
+			addToScene(mesh);
+			return mesh;
+		};
+
+		var xyz = function xyz(x, y, z) {
+			return new THREE.Vector3(x, y, z);
+		};
+		var direction_from_to = function direction_from_to( p0, p1 ) {
+        return p1.clone().sub( p0 ).normalize();
+    	};
+    	var point_distance = function point_distance( p0, p1 ) {
+        return p1.clone().sub( p0 ).length();
+	    };
+	    var add = function add( v0, v1 ) {
+	        return v0.clone().add( v1 );
+	    };
+	    var cross = function cross( v0, v1 ) {
+	        return v0.clone().cross( v1 );
+	    };
+	    var dot = function dot( v0, v1 ) {
+	        return v0.dot( v1 );
+	    };
+	    var linear_interpolation = function linear_interpolation( p0, p1, t ) {
+	        return p0.clone().lerp( p1, t );
+	    };
+
 		var move = function move(obj, x, y, z) {
 			var matrix = new THREE.Matrix4();
 		    matrix.makeTranslation( x, y, z );
+		    obj.applyMatrix( matrix );
+		    return obj;
+		};
+
+		var rotate = function rotate(obj, axis, angle) {
+			var matrix = new THREE.Matrix4();
+		    matrix.makeRotationAxis( axis, angle );
+		    obj.applyMatrix( matrix );
+		    return obj;
+		};
+
+		var scale = function scale(obj, x, y, z) {
+			var matrix = new THREE.Matrix4();
+		    matrix.makeScale( x, y, z );
 		    obj.applyMatrix( matrix );
 		    return obj;
 		};
@@ -87,6 +146,15 @@ define(["THREE"], function(THREE){
 			addToScene(empty);
 			return empty;
 		};
+
+		var parametric_surface = function parametric_surface(func, uSlices, vSlices) {
+		    var material = surfaceMaterial();
+		    var geometry = new THREE.ParametricGeometry( func, uSlices, vSlices );
+		    var mesh = new THREE.Mesh( geometry, material );
+		    addToScene(mesh);
+		    return mesh;
+		};
+
 
 		function evaluate(programText) {
 			_scene = new THREE.Scene();
