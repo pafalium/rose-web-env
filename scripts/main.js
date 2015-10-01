94cc648496153fe4332ce5920231febf6c882157
pafalium <pedroalfaiate1992@hotmail.com>
2015-10-01 17:44:09 +0100

    main.js:  - Yet another cleanup of the entry point.

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ require.config({
     }
 });
 
-require(["app/page", "app/old-evaluator", "app/eval-manager"], function(page, Evaluator, EvalManager) {
+require(["app/page"], function(page) {
     "use strict";
     /*
         Somehow this function should initialize the prototype.
@@ -28,44 +28,5 @@ require(["app/page", "app/old-evaluator", "app/eval-manager"], function(page, Ev
             the actual user interface assembly (or do it itself);
     */
 
-    //var program = page.editor.getValue();
-    //var resultScene = evaluator.executeProgram(program);
-    //page.view.showScene(resultScene);
-
-    //var cube_geom = new THREE.BoxGeometry(1,1,1);
-    //var mat = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    //var cube = new THREE.Mesh(cube_geom, mat);
-    //var scene = new THREE.Scene();
-    //scene.add(cube);
-    //page.view.setScene(scene);
-    /*
-    var evaluateProgram = function evaluateProgram() {
-        var program = page.editor.getValue();
-        var evaluator = new Evaluator();
-        var scene = evaluator.evaluate(program);
-        page.view.setScene(scene);
-    };
     
-    var setDelayedProgramEval = (function() {
-        var evaluationScheduled = false;
-        return function setDelayedProgramEval() {
-            if(!evaluationScheduled) {
-                evaluationScheduled = true;
-                setTimeout(function(){
-                    evaluationScheduled = false;
-                    evaluateProgram();
-                });
-            }
-        };
-    })();
-
-    page.editor.on("change", function(event){
-        setDelayedProgramEval();
-    });
-
-    setDelayedProgramEval();
-    */
-
-    var evalManager = new EvalManager(page.editor, page.view);
-    document.body.appendChild(evalManager.userInterface.managerControlsBar.domElement);
 });
